827bdd2004e0a7f5a09aabfe75d38ae3101fe2cc
MarianaSiano <marianasianop@gmail.com>
2024-10-30 10:52:47 -0300

    'App.jsx' terminado

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,2 +1,26 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import NavBar from '../Components/NavBar';
+import Home from '../Pages/Home';
+import Portfolio from '../Pages/Portfolio';
+import Curriculo from '../Pages/Curriculo';
+import Contato from '../Pages/Contato';
+import '../CSS/App.css';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/curriculo" element={<Curriculo />} />
+          <Route path="/contato" element={<Contato />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
