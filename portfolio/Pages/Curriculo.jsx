bc89b0cf1f9dd4979334e73ee70f5598881430d2
MarianaSiano <marianasianop@gmail.com>
2025-03-01 20:52:35 -0300

    Criacao do css 'portfolio' e parte do arquivo jsx do mesmo praticamente pronto

---
+++ portfolio/Pages/Curriculo.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import '../css/curriculo.css';
+
+function Curriculo() {
+    return(
+            <div className="div main">
+                <ul className='caminho'>
+                    <li><a href='/'>Home</a></li>
+                    <li>' '-- ' '</li>
+                    <li>Currículo</li>
+                </ul>
+                <h1>Currículo</h1>
+            </div>
+        );
+}
+
+export default Curriculo
